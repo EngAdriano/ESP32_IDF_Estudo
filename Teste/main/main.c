165909361b405a551c980b1909eb5c5e873db6a5
José Adriano <jose.filho@dellead.com>
2023-01-23 08:11:09 -0300

    Update main.c

--- Teste/main/main.c
+++ Teste/main/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/Task.h"
+#include "driver/gpio.h"
+
+#define PIN 2
 
 void app_main(void)
 {
-    
+    esp_rom_gpio_pad_select_gpio(PIN);
+    gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+
+    bool status = true;
+
+    while(true)
+    {
+        status = !status;
+        gpio_set_level(PIN, status);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
